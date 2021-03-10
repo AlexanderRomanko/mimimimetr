e640bf4834f346021073b01c8c8d9c6b3ce3ed5f
Alexander <alexander.romanko.job@gmail.com>
2021-03-10 10:48:04 -0900

    Check if collection size is multiple 2

--- src/main/java/com/example/mimimi/controller/VoteController.java
+++ src/main/java/com/example/mimimi/controller/VoteController.java
@@ -2,6 +2,7 @@ package com.example.mimimi.controller;
 
 import com.example.mimimi.domain.Cat;
 import com.example.mimimi.repos.CatRepository;
+import org.apache.tomcat.util.http.fileupload.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
@@ -10,6 +11,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.*;
 
 @Controller
@@ -23,22 +26,45 @@ public class VoteController {
     private CatRepository catRepository;
 
     @GetMapping("/chooseCollection")
-    public String showCollectionsList(Model model) {
-        List<Cat> collectionsList = catRepository.findAllTags();
-        model.addAttribute("collectionsList", collectionsList);
-        System.out.println(collectionsList);
+    public String showCollectionsList(Model model) throws IOException {
+        File upload = new File(uploadPath);
+        for (String file : upload.list()) {
+            File dir = new File(uploadPath + "/" + file);
+            if (dir.list().length % 2 > 0) {
+                FileUtils.deleteDirectory(dir);
+                catRepository.deleteByTag(file);
+            }
+        }
+        List<String> tags = new ArrayList<>();
+        catRepository.findDistinctCatsWithDistinctTags().forEach(x -> tags.add(x.getTag()));
+        for (String tag : tags) {
+            if (catRepository.findByTag(tag).size() % 2 > 0) {
+                File uploadDir = new File(uploadPath + "/" + tag);
+                FileUtils.deleteDirectory(uploadDir);
+                catRepository.deleteByTag(tag);
+            }
+        }
+//        for (String tag : tags) {
+//            List<Cat> cats = catRepository.findByTag(tag);
+//            if (cats.size() % 2 > 0) {
+//                for (Cat cat :
+//                        cats) {
+//                    catRepository.deleteById(cat.getId());
+//                }
+//            }
+//        }
+        model.addAttribute("collectionsList", catRepository.findDistinctCatsWithDistinctTags());
         return "chooseVoteCollection";
     }
 
     @GetMapping("/{tag}")
     public String vote(@PathVariable String tag, Model model) {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        Object pricipal = auth.getPrincipal();
         List<Cat> cats = catRepository.findByTag(tag);
-        cats.get(0).getVotedUsers().add("author");
-        if (cats.get(0).getVotedUsers().contains(pricipal)) {
-            return "vote";
-        } else {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        Object principal = auth.getPrincipal();
+        System.out.println(principal);
+        cats.removeIf(cat -> cat.getVotedUsers().contains(principal));
+        if (!cats.get(0).getVotedUsers().contains(principal)) {
             Random random = new Random();
             List<Cat> twoCats = new ArrayList<>();
             for (int i = 0; i < 2; i++) {
@@ -52,14 +78,25 @@ public class VoteController {
             model.addAttribute("cat1", twoCats.get(0));
             model.addAttribute("cat2", twoCats.get(1));
 //        model.addAttribute("filter", tag);
-            return "vote";
         }
+        return "vote";
 
     }
-    @PostMapping("/{tag}")
-    public String vote(@PathVariable String tag, @RequestParam Cat cats, Model model) {
-        return "vote";
-    }
+//    @PostMapping("/{tag}")
+//    public String vote(@PathVariable String tag, @RequestParam List cats, Model model) {
+//        if (cats.isEmpty()) return "redirect:/vote/{tag}/results";
+//        Random random = new Random();
+//        List<Cat> twoCats = new ArrayList<>();
+//        for (int i = 0; i < 2; i++) {
+//            int randomIndex = random.nextInt(cats.size());
+//            twoCats.add(cats.get(randomIndex));
+//            cats.remove(randomIndex);
+//        }
+//        model.addAttribute("cats", cats);
+//        model.addAttribute("cat1", twoCats.get(0));
+//        model.addAttribute("cat2", twoCats.get(1));
+//        return "vote";
+//    }
 
 //    @PostMapping
 //    public String addCollection(@RequestParam String tag, @RequestParam String name, @RequestParam("file") MultipartFile file,
--- src/main/java/com/example/mimimi/repos/CatRepository.java
+++ src/main/java/com/example/mimimi/repos/CatRepository.java
@@ -2,26 +2,26 @@ package com.example.mimimi.repos;
 
 import com.example.mimimi.domain.Cat;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Set;
 
 public interface CatRepository extends JpaRepository<Cat, Long> {
-    List<Cat> findByTag(String filter);
-//    Set<Cat> find
+    List<Cat> findByTag(String tag);
 
+
+    @Modifying
+    @Query(value = "delete from cat t where t.tag = :tags", nativeQuery = true)
+    List<Integer> deleteByTag(@Param("tags") String tags);
+//    void deleteNotFull(@Param("tags") String tags);
     //    @Query(value = "select id from cat order by random();", nativeQuery = true)
 //    List<Long> findAllIds();
 //    @Query(value = "select distinct(tag) from cat", nativeQuery = true)
 //    List<String> findAllTags();
     @Query(value = "SELECT * FROM cat WHERE id NOT IN (SELECT d2.id FROM cat AS d1 INNER JOIN cat AS d2 ON d2.tag=d1.tag WHERE d2.id > d1.id)", nativeQuery = true)
-//    @Query(value = "select distinct * from cat where id not in (select distinct tag)", nativeQuery = true)
-
-    List<Cat> findAllTags();
-
-
+    List<Cat> findDistinctCatsWithDistinctTags();
 
 //    Cat (String filename);
 }
