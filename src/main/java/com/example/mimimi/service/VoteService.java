9f252884bf8df1a1b91906d0d9989d57330c5c6c
Alexander <alexander.romanko.job@gmail.com>
2021-03-30 14:24:41 -0900

    refactoring added VoteService

---
+++ src/main/java/com/example/mimimi/service/VoteService.java
@@ -0,0 +1,66 @@
+package com.example.mimimi.service;
+
+import com.example.mimimi.entity.ComparableElement;
+import com.example.mimimi.repos.ComparableElementRepository;
+import org.apache.tomcat.util.http.fileupload.FileUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+import java.io.File;
+import java.io.IOException;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
+
+
+@Service
+public class VoteService {
+
+    @Value("${upload.path}")
+    private String uploadPath;
+
+//    private static String principal = SecurityContextHolder.getContext().getAuthentication().getName();
+
+    private final ComparableElementRepository comparableElementRepository;
+
+    public VoteService(ComparableElementRepository comparableElementRepository) {
+        this.comparableElementRepository = comparableElementRepository;
+    }
+
+    public List<ComparableElement> getComparableElements(String tag) {
+        List<ComparableElement> comparableElements = comparableElementRepository.findByTag(tag); // Each time calls repository
+        String principal = SecurityContextHolder.getContext().getAuthentication().getName();//if it's better to move this field up
+        comparableElements.removeIf(comparableElement -> comparableElement.getVotedUsers().contains(principal));
+        if (comparableElements.isEmpty()) return comparableElements;
+        Random random = new Random();
+        List<ComparableElement> twoComparableElements = new ArrayList<>();
+        for (int i = 0; i < 2; i++) {
+            int randomIndex = random.nextInt(comparableElements.size());
+            twoComparableElements.add(comparableElements.get(randomIndex));
+            comparableElements.remove(randomIndex);
+        }
+        return twoComparableElements;
+    }
+
+    public void vote(ComparableElement comparableElement1, ComparableElement comparableElement2, String button1) {
+        String principal = SecurityContextHolder.getContext().getAuthentication().getName();
+        if (button1 != null) comparableElement1.setLikes(comparableElement1.getLikes() + 1);
+        else comparableElement2.setLikes(comparableElement2.getLikes() + 1);
+        comparableElement1.getVotedUsers().add(principal);
+        comparableElement2.getVotedUsers().add(principal);
+        comparableElementRepository.save(comparableElement1);
+        comparableElementRepository.save(comparableElement2);
+    }
+
+    public List<ComparableElement> getResults(String tag) {
+        return new ArrayList<>(comparableElementRepository.findByTagOrderByLikesDesc(tag));
+    }
+
+}
